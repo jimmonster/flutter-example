21dbff12506672675cea8b7e779b2d5b83286bbc
jimmonster <851805729@qq.com>
2018-06-13 17:25:51 +0800

    example -initialNetwork

--- lib/tabs/first.dart
+++ lib/tabs/first.dart
@@ -5,34 +5,97 @@ import 'package:flutter_net/net/url/UrlPath.dart';
 import 'package:http/http.dart' as http;
 
 //界面布局
-class FirstTab extends StatelessWidget {
+class FirstTab extends StatefulWidget {
+  _FirstTabState createState() => _FirstTabState();
+}
+
+class _FirstTabState extends State<FirstTab> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text('布局样式'),
+          backgroundColor: Colors.lightGreenAccent,
+          centerTitle: true,
+        ),
+        body: new Container(
+            child: new Center(
+          child: new FutureBuilder<Bean>(
+              //加载数据
+              future: initData(),
+              //处理数据
+              builder: (context, responsedata) {
+                if (responsedata.hasData) {
+                  var data = responsedata.data.result;
+                  return _View(data);
+                } else if (responsedata.hasError) {
+                  return Text('获取数据失败：' + "${responsedata.error}");
+                } else {
+                  return new CircularProgressIndicator();
+                }
+              }),
+        )));
+  }
 
-        title:  Text('布局样式',),
-        centerTitle: true,
+  /**
+   * 加载网络数据
+   */
+  Future<Bean> initData() async {
+    var response = await http.get(UrlPath.FIRSTURL);
+    print("获取的数据：" + response.body.toString());
+    Map decode = json.decode(response.body);
 
-      ),
-        body: Container(
-          color: Colors.black12,
-          margin: new EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+    //解析数据
+    return Bean.forjson(decode);
+  }
 
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              new Image.asset('images/lake.jpg',
-                  width: 100.0, height: 100.0, fit: BoxFit.fill),
-              new Image.asset('images/lake.jpg',
-                  width: 100.0, height: 100.0, fit: BoxFit.fill),
-              new Image.asset('images/lake.jpg',
-                  width: 100.0, height: 100.0, fit: BoxFit.fill)
+  int index = 0;
 
-            ],
-          ),
+  Widget _View(List data) {
+    _addNum() {
+      setState(() {
+        index++;
+      });
+    }
 
+    return new ListView(
+      children: <Widget>[
+        Text(data[index]['title']),
+        Text(data[index]['des']),
+        new Image.network(
+          data[index]['pic'],
+          height: 400.0,
+        ),
 
-    ));
+        Text(
+          '当前页数：' + '${index}',
+          textDirection: TextDirection.rtl,
+        ),
+
+        //点击事件更新
+        new FloatingActionButton(
+            child: Text('下一页'),
+            tooltip: '下一页',
+            onPressed: _addNum,
+            backgroundColor: Colors.deepOrangeAccent),
+      ],
+    );
+  }
+}
+
+class Bean {
+  final int error_code;
+  final String reason;
+  final List result;
+
+  Bean({this.error_code, this.reason, this.result});
+
+  factory Bean.forjson(Map<String, dynamic> json) {
+    //将解析完成的数据返回
+    return Bean(
+      error_code: json['error_code'],
+      reason: json['reason'],
+      result: json['result'],
+    );
   }
 }
